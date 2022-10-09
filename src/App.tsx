0ab4b0884562dbf5800a16a61b5e17c90deebaa2
hbsnow <yuki.4uing@gmail.com>
2022-10-09 13:55:55 +0900

    feat: モーダル実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,54 @@
-import { HomePage } from "./pages/HomePage";
+import { useRef, useState } from "react";
+
+import styles from "./app.module.css";
 
 export const App = (): JSX.Element => {
-  return <HomePage />;
+  const ref = useRef<HTMLDialogElement>(null);
+
+  const [hidden, setHidden] = useState(true);
+
+  return (
+    <div>
+      <div className={styles.action}>
+        <button onClick={() => ref.current?.showModal()}>Open Modal 1</button>
+
+        <button
+          onClick={() => {
+            document.body.classList.add("open");
+            setHidden(false);
+          }}
+        >
+          Open Modal 2
+        </button>
+      </div>
+
+      {[...Array(100)].map((_, i) => (
+        <p key={i}>{i}</p>
+      ))}
+
+      <dialog className={styles.dialog} ref={ref}>
+        <p>Hello, world!</p>
+
+        <button onClick={() => ref.current?.close()}>Close Modal</button>
+      </dialog>
+
+      <div
+        className={styles.divDialog}
+        hidden={hidden}
+        role="dialog"
+        aria-modal="true"
+      >
+        <p>Hello, world!</p>
+
+        <button
+          onClick={() => {
+            document.body.classList.remove("open");
+            setHidden(true);
+          }}
+        >
+          Close Modal
+        </button>
+      </div>
+    </div>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 
+import "./main.css";
+
 const container = document.getElementById("root");
 if (!container) {
   throw new Error("Failed to find the root element");
