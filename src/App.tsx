655dce0e7652771d67c67efe5e2fd349089ca7cd
hbsnow <yuki.4uing@gmail.com>
2022-10-10 16:04:38 +0900

    fix: use dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 
 import styles from "./app.module.css";
 
 export const App = (): JSX.Element => {
-  const ref = useRef<HTMLDialogElement>(null);
+  const ref1 = useRef<HTMLDialogElement>(null);
+  const ref2 = useRef<HTMLDialogElement>(null);
 
-  const [hidden, setHidden] = useState(true);
+  // const [hidden, setHidden] = useState(true);
 
   return (
     <div>
       <div className={styles.action}>
-        <button onClick={() => ref.current?.showModal()}>Open Modal 1</button>
+        <button onClick={() => ref1.current?.showModal()}>Open Modal 1</button>
 
         <button
           onClick={() => {
+            console.log("122");
             document.body.classList.add("open");
-            setHidden(false);
+            ref2.current?.show();
           }}
         >
           Open Modal 2
@@ -26,29 +28,26 @@ export const App = (): JSX.Element => {
         <p key={i}>{i}</p>
       ))}
 
-      <dialog className={styles.dialog} ref={ref}>
+      <dialog className={styles.dialog} ref={ref1}>
         <p>Hello, world!</p>
 
-        <button onClick={() => ref.current?.close()}>Close Modal</button>
+        <button onClick={() => ref1.current?.close()}>Close Modal</button>
       </dialog>
 
-      <div
-        className={styles.divDialog}
-        hidden={hidden}
-        role="dialog"
-        aria-modal="true"
-      >
-        <p>Hello, world!</p>
+      <dialog className={styles.divDialog} ref={ref2}>
+        <div>
+          <p>Hello, world!</p>
 
-        <button
-          onClick={() => {
-            document.body.classList.remove("open");
-            setHidden(true);
-          }}
-        >
-          Close Modal
-        </button>
-      </div>
+          <button
+            onClick={() => {
+              document.body.classList.remove("open");
+              ref2.current?.close();
+            }}
+          >
+            Close Modal
+          </button>
+        </div>
+      </dialog>
     </div>
   );
 };
